fc9217e49e6d6a1a1549b17af6f3094b81ef8455
aswanym <aswanymahendran222@gmail.com>
2022-07-14 22:44:31 +0530

    heroku configurations

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$!fk!hr-%dn1cir1zfim7b6+zlkfw9d5@*$b4-%nuh%6yupy%7'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -138,3 +142,6 @@ DATABASE_ROUTERS = ['routers.db_routers.OrderRouter','routers.db_routers.Product
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
