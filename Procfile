dcc2162aefef77d4cc741165e9106f062e48c862
aswanym <aswanymahendran222@gmail.com>
2022-07-21 01:02:52 +0530

    fix for heroku deployment

--- Procfile
+++ Procfile
@@ -1,5 +1,4 @@
 release: python manage.py migrate --database=default
 release: python manage.py migrate --database=order_db
 release: python manage.py migrate --database=product_db
-
 web: gunicorn core.wsgi
--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import django_heroku
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -162,7 +163,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import dj_database_url
+
 DATABASES = {'default': dj_database_url.config()}
 
 django_heroku.settings(locals())
