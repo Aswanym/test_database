1ec92a7d6c08be0645890f7a20a2b8e19692ab98
aswanym <aswanymahendran222@gmail.com>
2022-07-22 00:05:26 +0530

    new settings

--- core/settings.py
+++ core/settings.py
@@ -163,7 +163,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 
 django_heroku.settings(locals())
